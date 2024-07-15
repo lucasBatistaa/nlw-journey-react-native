01175e4e2d947cf299698a2d6eadade079e91b84
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-07-14 22:39:27 -0300

    init first class

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+
+    return (
+        <View>
+            <Text>
+                Hello World!
+            </Text>
+        </View>
+    )
+}
