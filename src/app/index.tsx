e92b1bc59e4bbdd4559660bca8de30e533b180c5
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-07-15 23:13:40 -0300

    install and configurate libs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,9 @@ import { View, Text } from 'react-native'
 export default function Index() {
 
     return (
-        <View>
-            <Text>
-                Hello World!
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-zinc-400'>
+                Convide seus amigos e planeje sua próxima viagem
             </Text>
         </View>
     )
---
+++ src/components/loading.tsx
@@ -0,0 +1,5 @@
+import { ActivityIndicator } from "react-native";
+
+export function Loading() {
+    return <ActivityIndicator className="flex-1 bg-zinc-900 items-center justify-center text-lime-300" />
+}
